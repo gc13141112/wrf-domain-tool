75d06973fa80304aff7231c6e4f1b8f88f1a7b79
叩一人 <gaochao@kouyirendeMacBook-Pro.local>
2025-05-03 16:00:35 +0800

    add docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,13 +14,12 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
-templates_path = ['_templates']
-exclude_patterns = [
-    'otherextension',
+extensions = [    'otherextension',
     '...' ,
     'recommonmark',]
+
+templates_path = ['_templates']
+exclude_patterns = []
 
 
 
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,5 +1,5 @@
 .. wrf-domain-tool documentation master file, created by
-   sphinx-quickstart on Sat May  3 15:12:46 2025.
+   sphinx-quickstart on Sat May  3 15:58:40 2025.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
