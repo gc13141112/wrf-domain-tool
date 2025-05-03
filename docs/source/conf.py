5ee06f2c6006d365e294934ea3c3352d9b742f8d
叩一人 <gaochao@kouyirendeMacBook-Pro.local>
2025-05-03 16:14:18 +0800

    conf.py update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,30 +1,56 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# conf.py — Sphinx configuration file
+
+import os
+import sys
+
+# -- Path setup --------------------------------------------------------------
+
+# Add project root or relevant source directory to sys.path
+sys.path.insert(0, os.path.abspath('../../wrf-domain-tool'))
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'wrf-domain-tool'
-copyright = '2025, Chao Gao'
-author = 'Chao Gao'
-release = '0.1.0'
+author = 'Your Name or Organization'
+release = '0.1.0'  # Update with your version
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [ 
-    '...' ,
-    'recommonmark',]
+extensions = [
+    'sphinx.ext.autodoc',            # Automatic documentation from docstrings
+    'sphinx.ext.napoleon',           # Google and NumPy style docstring support
+    'sphinx.ext.viewcode',           # Add links to highlighted source code
+    'sphinx_autodoc_typehints',      # Use type hints in function signatures
+]
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# Autodoc settings
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'private-members': False,
+    'show-inheritance': True,
+}
 
+# Napoleon settings (optional tweaks)
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_use_param = True
+napoleon_use_rtype = True
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_theme_options = {
+    'navigation_depth': 4,
+    'collapse_navigation': False,
+}
+
+# -- Options for typehints ---------------------------------------------------
+
+typehints_fully_qualified = False  # Display just class names, not full paths
