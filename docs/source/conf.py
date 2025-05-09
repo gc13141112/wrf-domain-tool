427093142ef06d3878f1ce2d417b3d02f5881fca
叩一人 <gaochao@kouyirendeMacBook-Pro.local>
2025-05-09 20:31:12 +0800

    去除build以及添加自定义版权

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,6 +47,10 @@ napoleon_use_rtype = True
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_show_sphinx = False  # 去除“Built with Sphinx”
+html_show_copyright = False  # 不显示默认版权（我们自定义）
+templates_path = ['_templates']
+
 html_theme_options = {
     'navigation_depth': 4,
     'collapse_navigation': False,
