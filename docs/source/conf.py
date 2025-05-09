dec342bfdec6fb863c00c5111ca6cbf212b23128
叩一人 <gaochao@kouyirendeMacBook-Pro.local>
2025-05-09 20:42:36 +0800

    去除build以及添加自定义版权

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,6 @@ extensions = [
     'sphinx_autodoc_typehints',      # Use type hints in function signatures
 ]
 
-templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # Autodoc settings
